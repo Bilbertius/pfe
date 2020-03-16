63155846a1605a9c0b5060d547075ba2ca3f7dee
Bilbertius <lauchlinmacneil@gmail.com>
2020-03-16 00:43:34 -0600

    ok

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-   // API_ENDPOINT: "https://petful-server.herokuapp.com/api",
-    API_ENDPOINT: "http://localhost:8080/api"
+    API_ENDPOINT: "https://petful-server.herokuapp.com/api",
+
 }
 
 //http://localhost:8080/api
