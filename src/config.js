cdaa8176dd660b79fc76bc3dad853231258d8bfd
Bilbertius <lauchlinmacneil@gmail.com>
2020-04-18 00:56:30 -0600

    f

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || "http://localhost:8080/api"
+    API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || "https://petful-pearl.herokuapp.com/api"
     
 }
 
