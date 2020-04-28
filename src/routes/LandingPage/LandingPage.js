f4511012694a0e2f7e22b9c23b28bdf96baac5ad
Kraig Williams <47580137+kraigwilliams@users.noreply.github.com>
2020-04-28 04:30:05 -0400

    edit copy

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -37,8 +37,8 @@ class LandingPage extends React.Component {
     
           <p>Welcome to Petful where we pride ourselves as being the friendliest pet center in the world</p>
           <p>We have lovable pets for adoption. Come see which furry friend will touch your heart.</p>
-       <p>The process begins when  users enter their name and are placed in a line based who arrived first. Then you can see your place 
-       in the queue and when it is your turn select cat or dog you would like to adopt. Our policy is that the animals who were here first 
+       <p>The way our process works you first enter your name. You are placed in a queue with other adopters based on your arrival time.  You can then see your place 
+       in line and when it is your turn select the cat or dog you would like to adopt. Our policy is that the animals who were here first 
        are always the first to be adopted</p>
           <span>
                     <img id='landing-img' src={SearchPets} alt="search for your new pet"/>
